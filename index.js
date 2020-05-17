e5d4db3e622a060552d0a08744a2c49e7145f13b
Zabrina Brodale <zabrinaisis@gmail.com>
2020-05-17 17:48:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function theBeatlesPlay(artistArray, instrumentArray) {
-  
+  stringArray = [];
+  for (let i = 0; i < artistArray.length; i++) {
+    stringArray.push(artistArray[i] + " plays " + instrumentArray[i]);
+  }
+  return (stringArray);
 }
